38a600057ed2b82cf9abacb9332209ffde7b0dd6
IMakeBotsForYou <40301997+IMakeBotsForYou@users.noreply.github.com>
2021-02-09 20:54:38 +0200

    Update server_chatroom.py

--- server_chatroom.py
+++ server_chatroom.py
@@ -1,52 +1,193 @@
 """Server for multithreaded (asynchronous) chat application."""
+from re import search
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
+import requests as req
 
 
 def accept_incoming_connections():
     """Sets up handling for incoming clients."""
     while True:
         client, client_address = SERVER.accept()
-        print("%s:%s has connected." % client_address)
-        client.send("Greetings from the cave! Now type your name and press enter!".encode())
+        print(f"{client_address} has connected.")
+        client.send("Greetings from the cave! Now type your name and press enter!\n".encode() +
+                    "Enter /help or /commands to see the command list!".encode())
         addresses[client] = client_address
         Thread(target=handle_client, args=(client,)).start()
 
 
+command_list = "Command List:\n" + \
+               "/nick <new name>: Rename Yourself!\n" + \
+               "/whisper <name>: whisper to someone\n" + \
+               "/online: show who's online!\n" + \
+               "/login <password>: Try logging in as admin\n"
+
+admin_cmd_list = "/end, or /close -> closes server.\n" + \
+                 "/boot or /kick -> kicks a user by username\n"
+
+admin_password = "mike"
+
 COMMAND_PREFIX = "/"
 
 
-def get_client(val):
-    for key, value in clients.items():
-        if val == value:
-            return key
+def get_client(val, ip=False):
+    """
+    :param val: name to search
+    :param ip: Search by ip? (default off)
+    """
+    if ip:
+        for key, value in addresses.items():
+            if val == value[0]:
+                return key
+    else:
+        for key, value in clients.items():
+            if val == value[0]:
+                return key
     return "invalid"
 
+
+def retrieve_key(client):
+    """
+    Asks the client for its current key.
+    :param client: The client being asked.
+    """
+    client.send(("What is the key?".encode()))
+    return int(client.recv(BUFSIZ).decode())
+
+
+def kick(client, delete=True, close=False):
+    """
+    :param client: The client being kicked/removed
+    :param delete: Delete the client from the client list
+    :param close:  The server is being closed, so no need to say who left.
+    Kicks a user.
+    """
+    client.send("{System} Kicked".encode())
+    if not close:
+        broadcast(("{System} " + "%s has left the chat." % clients[client][0]).encode())
+    if delete:
+        print(f"Deleted {clients[client]}")
+        del clients[client]
+    client.close()
+
+
+def close_server():
+    # If admin, close
+    for x in clients:
+        try:
+            kick(x, False, True)
+        except OSError:
+            pass
+    lst = list(clients.keys()).copy()
+    for x in lst:
+        clients.pop(x, None)
+    exit(0)
+    quit(0)
+
+
 def handle_command(msg, client):
+    """
+    :param msg: The message
+    :param client: The client being handled.
+    If the user has sent a command, instead of a normal message -
+    we need to handle it properly.
+    """
+
+    print(f'Client {clients[client][0]}, {"is" if clients[client][1] else "is not"} an admin.')
+    print(f'They have executed the command {msg}')
+    # For me to know what you're up to.
+
+    # split command into args
     args = msg.strip().split(" ")
+    # first arg is the command name, after the '/'
     command = args[0][1:]
-    print(command)
+
+    '''
+    Admin commands
+    '''
+    if clients[client][1]:
+        if command == "kick":
+            recipient_name = ''.join([chr(ord(a) ^ retrieve_key(client)) for a in ''.join(args[1:])])
+
+            # Are we kicking an ip or a name?
+            if search(r"(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?\.){4}"[:-1], recipient_name):
+                recipient = get_client(recipient_name, True)
+            else:
+                recipient = get_client(recipient_name)
+
+            if recipient != "invalid":
+                kick(recipient)
+            else:
+                return f'That recipient is not connected.'.encode()
+
+    '''
+    Regular commands
+    '''
+    if command == "close" or command == "end":
+        if clients[client][1]:
+            close_server()
+        else:
+            return 'You can\'t do that!'.encode()
+
+    '''
+    Logging in as admin
+    We need to check if user is already an adimn or not,
+    and recieve the password.
+    '''
+    if command == "login":
+        if clients[client][1]:
+            client.send(("{System} " "You're already admin! Want to log out? y/N".encode()))
+            login = client.recv(BUFSIZ).decode().lower()
+            if login == "y" or login == "yes":
+                clients[client][1] = False
+            else:
+                return "Canceled."
+        else:
+            print(f"Logging {clients[client][0]} in")
+            key = req.get("https://get-api-key-2021.herokuapp.com/").json()['code']
+
+            passw = ''.join([chr(ord(a) ^ key) for a in ''.join(args[1:])])
+            clients[client] = [clients[client][0], passw == admin_password]
+
+            print('Logged in!' if passw == admin_password else "Login failed.")
+            return 'Logged in!'.encode() if passw == admin_password else "Login failed.".encode()
+
     if command == "w" or command == "whisper":
         recipient_name = args[1]
         recipient = get_client(recipient_name)
-        print(recipient)
+        if recipient == client:
+            return "You can't message yourself, dummy".encode()
+
         if recipient != "invalid":
-            recipient.send(("{System} Message from " + clients[client] + ": " + ' '.join(args[2:])).encode())
+            recipient.send(("{System} Message from " + clients[client][0] + ": " + ' '.join(args[2:])).encode())
+        else:
+            return f'That recipient is not connected.'.encode()
+
         return f'Direct message to: {recipient_name} - {" ".join(args[2:])}'.encode()
+
     if command == "help" or command == "commands":
-        return " Command List".encode()
+        return (command_list + admin_cmd_list).encode() if clients[client][1] else command_list.encode()
+
     if command == "nick" or command == "nickname":
         if len(args) > 1:
-            prev_name = clients[client]
-            clients[client] = "".join(word for word in args[1:] if word != "")
-            broadcast(("{System} " + f'{prev_name} changed to {clients[client]}').encode())
-            return "Nickname Updated.".encode()
+
+            recipient_name = args[1]
+            recipient = get_client(recipient_name)
+            print(recipient)
+
+            if recipient == "invalid":
+                prev_name = clients[client][0]
+                clients[client][0] = "_".join(word for word in args[1:] if word != "")
+                broadcast(("{System} " + f'{prev_name} changed to {clients[client][0]}').encode())
+                return "Nickname Updated.".encode()
+            else:
+                return f'That name is taken.'.encode()
         else:
             return "Must enter a nickname.".encode()
 
     # Currently online users
     if command == "current" or command == "online":
-        return (str(len(clients)) + ' users online, ').encode() + ' | '.join(clients.values()).encode()
+        return (str(len(clients)) + ' users online, ').encode() + ' | '.join([x[0] for x in clients.values()]).encode()
     return "Invalid Command".encode()
 
 
@@ -59,37 +200,37 @@ def handle_client(client):  # Takes client socket as argument.
         print("error")
     else:
         connection_working = True
+        print("Handling client")
         welcome = "{System} " + 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
         client.send(welcome.encode())
         msg = "{System} " + "%s has joined the chat!" % name
         broadcast(msg.encode())
-        clients[client] = name
-
+        clients[client] = [name.replace(" ", "_"), False]
         while True:
             try:
                 msg = client.recv(BUFSIZ)
             except ConnectionResetError:  # 10054
                 connection_working = False
+            except ConnectionAbortedError:
+                connection_working = False
             if connection_working and msg != "quit()".encode():
                 if chr(msg[0]).encode() == COMMAND_PREFIX.encode():
-                    print(f'Command executed by {clients[client]}, {str(msg)}')
+                    print(f'Command executed by {clients[client][0]}, {str(msg)}')
                     client.send("{System} ".encode() + handle_command(msg.decode(), client))
                 else:
-                    broadcast(msg, clients[client] + ": ")
+                    broadcast(msg, clients[client][0] + ": ")
             else:
                 try:
-                    client.send("quit()".encode())
+                    kick(client)
                 except ConnectionResetError:  # 10054
                     connection_working = False
-                client.close()
-                del clients[client]
-                broadcast(("{System} " + "%s has left the chat." % name).encode())
+                except OSError:
+                    connection_working = False
                 break
 
 
 def broadcast(msg, prefix=""):  # prefix is for name identification.
     """Broadcasts a message to all the clients."""
-
     for sock in clients:
         try:
             sock.send(prefix.encode() + msg)
